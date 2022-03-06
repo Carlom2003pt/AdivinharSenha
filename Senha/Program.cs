3215c5bc21ff47f3446ba9db93a25472deb1c5f9
Carlom2003pt <carlom634@gmail.com>
2022-03-06 10:28:02 +0000

    AdivinheSenha

---
+++ Senha/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            do
+            {
+                Console.Write("Insira a senha: ");
+                numero = double.Parse(Console.ReadLine());
+                if (numero == 2002)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Acesso Permitido");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Senha Invalida");
+                }     
+            } while (numero != 2002);
+
+        }
+    }
+}
